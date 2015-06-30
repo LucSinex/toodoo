9782bc88bb175387d8483bbd14c9077ffc0dfb3e
Luc Sinex <lucsinex@gmail.com>
2015-06-30 17:47:40 -0400

    Create list link added to index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,9 +1,29 @@
 class ListsController < ApplicationController
-  def show
-  end
-
   def index
   	@lists = List.all
   	@numrows = 1 + @lists.count / 3
   end
+
+  def show
+  	@list = List.find(params[:id])
+  end
+
+  def new
+  	@list = List.new
+  end
+
+  def create
+  	@list = List.new(list_params)
+  	if @list.save
+  		redirect_to root_path
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  	def list_params
+  		params.require(:list).permit(:title)
+  	end
 end
